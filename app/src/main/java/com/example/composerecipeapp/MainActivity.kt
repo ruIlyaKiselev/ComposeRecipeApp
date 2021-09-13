55d2af6887c0fbe9ba1e9208721e1903b69a5e61
ilya <ruilyakiselev@gmail.com>
2021-09-13 22:42:20 +0700

    Add jetpack navigation between fragments

--- app/src/main/java/com/example/composerecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/composerecipeapp/MainActivity.kt
@@ -1,52 +1,13 @@
 package com.example.composerecipeapp
 
-import android.app.AppComponentFactory
-import android.content.res.Resources
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_container, RecipeListFragment())
-            .commit()
 
-    }
-
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        ComposeRecipeAppTheme {
-            Greeting("Android")
-        }
     }
 }
--- app/src/main/java/com/example/composerecipeapp/RecipeFragment.kt
+++ app/src/main/java/com/example/composerecipeapp/RecipeFragment.kt
@@ -5,48 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment: Fragment() {
-
+class RecipeFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return ComposeView(requireContext()).apply {
             setContent {
                 Column(
                     modifier = Modifier
                         .padding(16.dp)
                 ) {
-
                     Text(
-                        text = "Recipe List",
+                        text = "Recipe Fragment",
                         color = Color.Red,
-                        fontSize = 26.sp
+                        fontSize = 42.sp
                     )
-
-                    Spacer(modifier = Modifier.padding(16.dp))
-
-                    Button(
-                        onClick = {
-                            findNavController().navigate(R.id.viewRecipe)
-                        }
-                    ) {
-                        Text(text = "To Recipe Fragment!")
-                    }
                 }
             }
         }

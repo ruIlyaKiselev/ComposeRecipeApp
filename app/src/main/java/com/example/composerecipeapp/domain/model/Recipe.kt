1c9bd8b45f85152be49216b78d9d2c753f6ffca4
ilya <ruilyakiselev@gmail.com>
2021-09-14 20:14:47 +0700

    Fix recipe domain entity

--- app/src/main/java/com/example/composerecipeapp/domain/model/Recipe.kt
+++ app/src/main/java/com/example/composerecipeapp/domain/model/Recipe.kt
@@ -5,17 +5,15 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Recipe(
-//    val cooking_instructions: Any?,
-    val date_added: String?,
-    val date_updated: String?,
-    val description: String?,
-    val featured_image: String?,
-    val ingredients: List<String>?,
-    val long_date_added: Int?,
-    val long_date_updated: Int?,
-    val pk: Int?,
+    val id: Int?,
+    val title: String?,
     val publisher: String?,
+    val featuredImage: String?,
     val rating: Int?,
-    val source_url: String?,
-    val title: String?
+    val sourceUrl: String?,
+    val description: String?,
+    val cooking_instructions: String?,
+    val ingredients: List<String>?,
+    val dateAdded: String?,
+    val dateUpdated: String?,
 ): Parcelable

23db36b0c12fb4ebd847a156376f446cbc31180b
ilya <ruilyakiselev@gmail.com>
2021-09-19 20:02:45 +0700

    added red heart animation instead of default circular progress bar

--- app/src/main/java/com/example/composerecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,13 +1,19 @@
 package com.example.composerecipeapp.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -34,14 +40,25 @@ fun CircularIndeterminateProgressBar(
                 modifier = Modifier.fillMaxSize()
             ) {
 
-                CircularProgressIndicator(
-                    modifier = Modifier.layoutId("progressBar"),
-                    color = MaterialTheme.colors.primary
+                Pulsating(
+                    content = {
+                        Image(
+                            modifier = Modifier
+                                .width(200.dp),
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(Red)
+                        )
+                    },
+                    layoutId = "pulsating",
+                    pulseFraction = 1.5f
                 )
-
+                
                 Text(
-                    modifier = Modifier.layoutId("text"),
-                    text = "Loading...",
+                    modifier = Modifier
+                        .layoutId("text")
+                        .padding(top = 8.dp),
+                    text = " Loading...",
                     style = TextStyle(
                         color = MaterialTheme.colors.onSurface,
                         fontSize = 16.sp
@@ -55,17 +72,19 @@ fun CircularIndeterminateProgressBar(
 private fun myDecoupledConstraints(verticalBias: Float): ConstraintSet {
     return ConstraintSet {
         val guideline = createGuidelineFromTop(verticalBias)
-        val progressBar = createRefFor("progressBar")
+        val pulsating = createRefFor("pulsating")
         val text = createRefFor("text")
 
-        constrain(progressBar) {
+
+
+        constrain(pulsating) {
             top.linkTo(guideline)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
         }
 
         constrain(text) {
-            top.linkTo(progressBar.bottom)
+            top.linkTo(pulsating.bottom)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
         }
--- app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -26,10 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.composerecipeapp.domain.model.Recipe
-import com.example.composerecipeapp.presentation.components.CircularIndeterminateProgressBar
-import com.example.composerecipeapp.presentation.components.RecipeCard
-import com.example.composerecipeapp.presentation.components.SearchAppBar
-import com.example.composerecipeapp.presentation.components.foodCategoryChip
+import com.example.composerecipeapp.presentation.components.*
 import com.example.composerecipeapp.ui.theme.ComposeRecipeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -39,6 +38,7 @@ class RecipeListFragment: Fragment() {
 
     private val viewModel: RecipeListViewModel by viewModels()
 
+    @ExperimentalAnimationApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -85,7 +85,6 @@ class RecipeListFragment: Fragment() {
 
                             CircularIndeterminateProgressBar(isDisplayed = loading)
                         }
-
                     }
                 }
             }

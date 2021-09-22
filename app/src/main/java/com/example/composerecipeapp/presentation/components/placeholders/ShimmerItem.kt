558389c7884cf9db34797581304a8317108eb151
ilya <ruilyakiselev@gmail.com>
2021-09-22 14:08:07 +0700

    create card loading placeholder (for main screen list)

---
+++ app/src/main/java/com/example/composerecipeapp/presentation/components/placeholders/ShimmerItem.kt
@@ -0,0 +1,32 @@
+package com.example.composerecipeapp.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShimmerItem(
+    brush: Brush
+) {
+    // Column composable comtaining spacer shaped like a rectangle,
+    // set the [background]'s [brush] with the brush receiving from [ShimmerAnimation]
+    // Composable which is the Animation you are gonna create.
+    Column(modifier = Modifier.padding(16.dp)) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(250.dp)
+                .background(brush = brush)
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+                .padding(vertical = 8.dp)
+                .background(brush = brush)
+        )
+    }
+}
--- app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.composerecipeapp.domain.model.Recipe
@@ -42,10 +51,10 @@ class RecipeListFragment: Fragment() {
                 ComposeRecipeAppTheme {
                     Column {
 
-                        AnimatedHeartButton(
-                            viewModel.isLiked.value,
-                            viewModel::onClickLike
-                        )
+//                        AnimatedHeartButton(
+//                            viewModel.isLiked.value,
+//                            viewModel::onClickLike
+//                        )
 
                         SearchAppBar(
                             query = query,
@@ -57,6 +66,8 @@ class RecipeListFragment: Fragment() {
                             newSearch = viewModel::newSearch,
                             clearFocus = { clearFocus() }
                         )
+
+                        ShimmerAnimation()
 
                         Box(
                             modifier = Modifier
